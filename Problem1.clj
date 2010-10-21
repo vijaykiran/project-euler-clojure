55d1e4394815af1c73dd400ea6443c99a26510cd
vijaykiran <mail@vijaykiran.com>
2010-10-21 15:30:28 +0200

    Much better with anonymous function

--- Problem1.clj
+++ Problem1.clj
@@ -2,28 +2,6 @@
 ;; The sum of these multiples is 23.
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(defn divisible-by-3-5 
-  "Tests if n is divisible by 3 or 5"
-  [n]
-  (cond	(zero? (rem n 3)) true
-				(zero? (rem n 5)) true
-				:else false))
-
-
-(apply + (filter divisible-by-3-5 (take 1000 ( range 1 1000))))
-
-
-;; mysumall - overr engineering due to ignorance :)
-
-; (defn mysumall 
-;   ([nums] (mysumall nums 0))
-; 	([nums sum]
-; 		(let [f (first nums)
-; 					r (rest nums)]
-; 				(if (zero? (count r))
-; 					(+ sum f)
-; 					(mysumall r (+ sum f))))))
-; 
-; 
-; (mysumall (filter divisible-by-3-5 (take 1000 ( range 1 1000))))
-
+(apply + (filter #(cond (zero? (rem % 3)) true
+												(zero? (rem % 5)) true
+												:else false) (take 1000 (range 1 1000))))
