c2fd0b8524cc877c3b822c09439359e0c45410f8
vijaykiran <mail@vijaykiran.com>
2010-10-26 22:29:37 +0200

    Added comments

--- Problem2.clj
+++ Problem2.clj
@@ -3,10 +3,13 @@
 ;   1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 ; Find the sum of all the even-valued terms in the sequence which do not exceed four million (4000000).
 
+
+;; fib-seq method from the clojure wiki
 (defn fib-seq []
   ((fn rfib [a b] 
        (cons a (lazy-seq (rfib b (+ a b)))))
     1 2))
 
 
+;; by manual trail & error found that 11 numbers are needed in the series
 (reduce + (take 11 (filter even? (fib-seq))))
