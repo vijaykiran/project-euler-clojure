7cc4adb09d06f77f1e86fe92d44272b1c4939321
vijaykiran <mail@vijaykiran.com>
2010-11-15 23:24:07 +0100

    docstring added

--- Problem3.clj
+++ Problem3.clj
@@ -1,7 +1,9 @@
 ;; The prime factors of 13195 are 5, 7, 13 and 29.
 ;; What is the largest prime factor of the number 600851475143 ?
 
-(defn factor [n i]
+(defn factor
+	"Finds out the largest factor for the given number n"
+	[n i]
   (if (= n i)
     n
     (if (zero? (mod n i))
